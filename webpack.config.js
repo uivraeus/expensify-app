01f424599c41012310f6427ad977913a2670b581
Ulf Ivraeus <ulf.ivraeus@gmail.com>
2018-04-27 16:00:37 +0200

    Add bundle.js and build for production

--- webpack.config.js
+++ webpack.config.js
@@ -7,31 +7,37 @@ const path = require('path');
 // babel-loader is a webpack variant of babel-cli, the presets are given by the .babelrc file
 // regarding devtools; look at documentation page at webpack.js.org - there are many different variants!
 // regarding css; "use" instead of "loader" - because it isn't one single loader (array is needed)
-module.exports = {
-  entry: './src/app.js',
-  //entry: './src/playground/hoc.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    },{
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+
+module.exports = (env) => {
+  console.log('env', env);
+  const isProduction = env === 'production';
+
+  return {
+    entry: './src/app.js',
+    //entry: './src/playground/hoc.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },{
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
 
