33239ce692dd60f2b2da9dc142edde472d389f82
Ulf Ivraeus <ulf.ivraeus@gmail.com>
2018-05-15 17:03:46 +0200

    Add styling to all pages

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import { startSetExpenses } from './actions/expenses';
 import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'; //webpack knows how to handle css
@@ -29,7 +30,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 
 firebase.auth().onAuthStateChanged((user) => {
